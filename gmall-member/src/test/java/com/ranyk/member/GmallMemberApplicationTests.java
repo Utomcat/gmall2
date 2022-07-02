6e11cb9efb7ef9d44a89d084364f2e721cd5dcf9
ranyk <1390851149@qq.com>
2022-07-02 17:00:15 +0800

    feat(gmall): 多模块初始化

---
+++ gmall-member/src/test/java/com/ranyk/member/GmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranyk.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-order/src/test/java/com/ranyk/order/GmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranyk.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
